a57091b8caf4d904d71ca2d520029fdbcb07e05d
StarIsCoder <654641639@qq.com>
2019-11-08 17:51:02 +0800

    Update UniquePaths.java - One array solution

--- src/UniquePaths.java
+++ src/UniquePaths.java
@@ -20,19 +20,33 @@ public class UniquePaths {
     }
 
     static public int uniquePaths(int m, int n) {
-        // 2D dynamic programming
-        int[][] array = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            array[i][0] = 1;
+//        // 2D dynamic programming
+//        int[][] array = new int[m][n];
+//        for (int i = 0; i < m; i++) {
+//            array[i][0] = 1;
+//        }
+//        for (int i = 0; i < n; i++) {
+//            array[0][i] = 1;
+//        }
+//        for (int i = 1; i < m; i++) {
+//            for (int j = 1; j < n; j++) {
+//                array[i][j] = array[i - 1][j] + array[i][j - 1];
+//            }
+//        }
+//        return array[m-1][n-1];
+
+        // Only one array, column by column/row by row
+        int big = Math.max(m, n);
+        int small = Math.min(m, n);
+        int[] dp = new int[small];
+        for (int i = 0; i < small; i++) {
+            dp[i] = 1;
         }
-        for (int i = 0; i < n; i++) {
-            array[0][i] = 1;
-        }
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                array[i][j] = array[i - 1][j] + array[i][j - 1];
+        for (int i = 0; i < big - 1; i++) {
+            for (int j = 1; j < small; j++) {
+                dp[j] = dp[j] + dp[j - 1];
             }
         }
-        return array[m-1][n-1];
+        return dp[n - 1];
     }
 }
